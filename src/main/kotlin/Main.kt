a13b66605a0b05cdd57063daac81655174c5d547
ahadkhaan97 <ahad.sartaj.ew@gmail.com>
2023-03-10 15:32:55 +0500

    submitted

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main() {
+    print(solution(mutableListOf(0, -1, 0, -1, 0, -1)))
+}
+
+fun solution(a: MutableList<Int>): Int {
+    val n = a.size
+    var count = 0
+    var sum = 0
+    val target = a.sum() / 3
+    var i = 0
+    val j = n - 1
+    while (i < j) {
+        sum += a[i]
+        if (sum == target) {
+            var k = j
+            var sum2 = 0
+            while (k > i + 1) {
+                sum2 += a[k]
+                if (sum2 == target) {
+                    count++
+                }
+                k--
+            }
+        }
+        i++
+    }
+    return count
+}
